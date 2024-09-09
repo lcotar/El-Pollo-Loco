238c1161e7ca9eb2b0ffee19316afadbafb903d4
Lea Cotar <lea.cotar@gmx.ch>
2024-09-09 16:02:06 +0200

    added more background

--- classes/world.class.js
+++ classes/world.class.js
@@ -19,6 +19,57 @@ class World {
       "assets/img/5_background/layers/1_first_layer/1.png",
       0
     ),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 719),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/3_third_layer/2.png",
+      719
+    ),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/2_second_layer/2.png",
+      719
+    ),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/1_first_layer/2.png",
+      719
+    ),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 719 * 2),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/3_third_layer/2.png",
+      719 * 2
+    ),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/2_second_layer/2.png",
+      719 * 2
+    ),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/1_first_layer/2.png",
+      719 * 2
+    ),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 719 * 3),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/3_third_layer/2.png",
+      719 * 3
+    ),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/2_second_layer/2.png",
+      719 * 3
+    ),
+
+    new BackgroundObject(
+      "assets/img/5_background/layers/1_first_layer/2.png",
+      719 * 3
+    ),
   ];
   canvas;
   ctx;
