06376396ad342d9c9f92267c81eb63bcc3f24e8c
Lea Cotar <lea.cotar@gmx.ch>
2024-09-10 11:21:35 +0200

    added sounds

--- classes/backgroud.class.js
+++ classes/backgroud.class.js
@@ -1,9 +1,17 @@
 class BackgroundObject extends MovableObject {
   width = 720;
   height = 480;
+  backgroundSound = new Audio("assets/audio/background_music.mp4");
+
   constructor(imagePath, x) {
     super().loadImage(imagePath);
     this.x = x;
     this.y = 480 - this.height;
+  }
+
+  animation() {
+    setInterval(() => {
+      this.backgroundSound.play();
+    });
   }
 }
