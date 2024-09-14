469d58a5bd78aea49580856012103d7e5b3d5793
Lea Cotar <lea.cotar@gmx.ch>
2024-09-14 11:54:12 +0200

    show jump animation

--- classes/character.class.js
+++ classes/character.class.js
@@ -10,6 +10,18 @@ class Character extends MovableObject {
     "assets/img/2_character_pepe/2_walk/W-25.png",
     "assets/img/2_character_pepe/2_walk/W-26.png",
   ];
+
+  IMAGES_JUMPING = [
+    "assets/img/2_character_pepe/3_jump/J-31.png",
+    "assets/img/2_character_pepe/3_jump/J-32.png",
+    "assets/img/2_character_pepe/3_jump/J-33.png",
+    "assets/img/2_character_pepe/3_jump/J-34.png",
+    "assets/img/2_character_pepe/3_jump/J-35.png",
+    "assets/img/2_character_pepe/3_jump/J-36.png",
+    "assets/img/2_character_pepe/3_jump/J-37.png",
+    "assets/img/2_character_pepe/3_jump/J-38.png",
+    "assets/img/2_character_pepe/3_jump/J-39.png",
+  ];
   world;
 
   walkingSound = new Audio("assets/audio/Walking_through_grass_(long).mp3");
@@ -17,6 +29,7 @@ class Character extends MovableObject {
   constructor() {
     super().loadImage("assets/img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.applyGravity();
     this.animation();
 
@@ -43,9 +56,13 @@ class Character extends MovableObject {
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        // Walk animation
-        this.playAnimation(this.IMAGES_WALKING);
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+          // Walk animation
+          this.playAnimation(this.IMAGES_WALKING);
+        }
       }
     }, 50);
   }
