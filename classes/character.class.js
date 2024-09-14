fee233c540a3f3edf0d067e18036019a7de33259
Lea Cotar <lea.cotar@gmx.ch>
2024-09-14 11:45:15 +0200

    add Gravity

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,6 +1,6 @@
 class Character extends MovableObject {
   height = 300;
-  y = 130;
+  y = 80;
   speed = 10;
   IMAGES_WALKING = [
     "assets/img/2_character_pepe/2_walk/W-21.png",
@@ -17,7 +17,7 @@ class Character extends MovableObject {
   constructor() {
     super().loadImage("assets/img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
-
+    this.applyGravity();
     this.animation();
 
     /* = new Animation(this.img, 100, 300, 50, 150, 6, 100);
