41995b2a2e5659bc0cf5b3eb50cb97eb47c336e2
Lea Cotar <lea.cotar@gmx.ch>
2024-09-18 19:23:28 +0200

    coins

--- classes/coin.class.js
+++
@@ -1,1 +0,0 @@
-class Coin extends MovableObject {}
---
+++ classes/coins.class.js
@@ -0,0 +1,10 @@
+class Coin extends MovableObject {
+  y = 100;
+  coins = [];
+  offset = {
+    top: 0,
+    bottom: 40,
+    left: 0,
+    right: 0,
+  };
+}
